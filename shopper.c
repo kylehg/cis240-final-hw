a138143b61b9a94d547f215a40d6ccb781dc8e61
Kyle Hardgrave <kyle@kylehardgrave.com>
2011-11-25 18:04:13 -0500

    Updated main logic to handle file-reading - isn't yet capitalization agnostic and has a bug for initial negative values

--- shopper.c
+++ shopper.c
@@ -1,5 +1,5 @@
 /**
- * main.c: Oh you know.
+ * shopper.c: The main logic.
  */
 
 #include <stdio.h>
@@ -9,41 +9,78 @@
 
 #define MAX_LINE_LENGTH 200
 
-int main() {
-
-  char input[MAX_LINE_LENGTH];
+int main(int argc, char *argv[]) {
+  FILE *input_file;
+  char line[MAX_LINE_LENGTH];
   char name[MAX_LINE_LENGTH];
   char num_str[MAX_LINE_LENGTH];
   int number;
   list_item *head = NULL;
 
-    printf("Enter command (update, print, quit):\n");
-  while (1) {
-    printf(">>> ");
-    fgets(input, MAX_LINE_LENGTH, stdin);
+  // Check for the correct number of args
+  if (argc != 2) {
+    printf("usage: %s filename.txt\n", argv[0]);
+    exit(1);
+  }
 
+  input_file = fopen(argv[1], "r");
+
+  // If fopen() returns a null pointer
+  if (input_file == 0) {
+    printf("Could not open file\n");
+    exit(1);
+  }
+
+  // Read each line of the file
+  while (fgets(line, MAX_LINE_LENGTH, input_file)) {
+
+    printf("Read: %s", line);
+    
     // Check for add command
-    if (sscanf(input, "update %s %s", name, num_str) == 2) {
+    if (sscanf(line, "%s %s", name, num_str) == 2) {
       number = atoi(num_str);
       head = update(head, name, number);
-      printf("%s: %d\n", name, number);
+      printf("...update(head, %s, %d\n", name, number);
+    } else {
+      printf("...ERROR: improperly formatted line\n");
+      exit(1);
     }
-
-    // Check for list command
-    if (input[0] == 'p') {
-      print_list (head);
-    }
-
-    // check for quit command
-    if (input[0] == 'q') {
-      while (head) {
-        head = delete(head, head);
-      }
-
-      exit(0);
-    }
-
   }
+
+  fclose(input_file);
+  print_list(head);
+
+  while (head) {
+    head = delete(head, head);
+  }
+
+//char input[MAX_LINE_LENGTH];
+//char name[MAX_LINE_LENGTH];
+//char num_str[MAX_LINE_LENGTH];
+//int number;
+//list_item *head = NULL;
+//
+//  printf("Enter command (update, print, quit):\n");
+//while (1) {
+//  printf(">>> ");
+//  fgets(input, MAX_LINE_LENGTH, stdin);
+//
+//
+//  // Check for list command
+//  if (input[0] == 'p') {
+//    print_list (head);
+//  }
+//
+//  // check for quit command
+//  if (input[0] == 'q') {
+//    while (head) {
+//      head = delete(head, head);
+//    }
+//
+//    exit(0);
+//  }
+//
+//}
 
   return 0;
 
