652620fb659a63157f8db700cf623c3f001f2b13
Kyle Hardgrave <kyle@kylehardgrave.com>
2011-11-28 00:10:28 -0500

    Added parameterized macros for extracting bits

--- memory.c
+++ memory.c
@@ -11,6 +11,26 @@
 #define MEM_LEN 65536
 #define REG_LEN 8
 
+#define I_OP(i)   ((i) >> 12) // For opcodes
+#define I_2_0(i)  ((i) & 0x7) //For Rt
+#define I_8_6(i)  (((i) >> 6) & 0x7) // For Rs
+#define I_11_9(i) (((i) >> 9) & 0x7) // For Rd, and NZP codes
+
+#define I_3_0(i)  ((i) & 0xF) // For IMM4 (AND w/ 15)
+#define I_4_0(i)  ((i) & 0x1F) // For IMM5 (AND w/ 31)
+#define I_5_0(i)  ((i) & 0x3F) // For IMM6 (AND w/ 63)
+#define I_6_0(i)  ((i) & 0x7F) // FOR IMM7 (AND w/ 127)
+#define I_7_0(i)  ((i) & 0xFF) // FOR IMM8 (AND w/ 255)
+#define I_8_0(i)  ((i) * 0x1FF) // FOR IMM9 (AND w/ 511)
+#define I_10_0(i) ((i) * 0x7FF) // FOR IMM11 (AND w/ 2047)
+
+#define I_5(i)    (((i) >> 5) & 0x1) // For 1-bit secondary opcodes
+#define I_5_4(i)  (((i) >> 4) & 0x2) // For 2-bit secondary opcodes
+#define I_5_3(i)  (((i) >> 3) & 0x7) // For 3-bit secondary opcodes
+#define I_11(i)   (((i) >> 11) & 0x1) // For 1-bit secondary opcodes in I[11]
+
+
+
 int mem[MEM_LEN];
 int reg[REG_LEN];
 
@@ -31,10 +51,10 @@ void print_lc4_state() {
   fputs("<<<<<<<<<<<<<<<<<\n", f);
 
   for (r = 0; r < REG_LEN; r++)
-    fprintf(f, "R%d: %x\n", r, reg[r]);
+    fprintf(f, "R%d: %x :: ", r, reg[r]);
 
-  fputs("\n>>> MEMORY <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<", f);
-  fputs("<<<<<<<<<<<<<<<<<\n", f);
+  fputs("\n\n>>> MEMORY <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<", f);
+  fputs("<<<<<<<<<<<<<<<<<<<\n", f);
 
   for (m = 0; m < MEM_LEN; m++) {
 
