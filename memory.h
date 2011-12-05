277618abe23e1662daa31d51ec1f953f64c28f82
Kyle Hardgrave <kyle@kylehardgrave.com>
2011-12-05 18:57:16 -0500

    Bringing .h totally in line - I'm dumb

--- memory.h
+++ memory.h
@@ -3,7 +3,7 @@
 
 int mem_store(char type, unsigned short addr, unsigned short word);
 
-void do_br(short imm9, int nzp);
+void do_br(int nzp, short imm9);
 
 void do_add(int rd, int rs, int rt);
 void do_mul(int rd, int rs, int rt);
@@ -14,7 +14,7 @@ void do_addi(int rd, int rs, short imm5);
 void do_cmp(int rs, int rt);
 void do_cmpu(int rs, int rt);
 void do_cmpi(int rs, short imm7);
-void do_cmpiu(int rs, short uimm7);
+void do_cmpiu(int rs, unsigned short uimm7);
 
 void do_jsr(short imm11);
 void do_jsrr(int rs);
@@ -25,23 +25,27 @@ void do_or(int rd, int rs, int rt);
 void do_xor(int rd, int rs, int rt);
 void do_andi(int rd, int rs, short imm5);
 
-void do_ldr(int rd, int rs, short imm6);
+void do_ldr(int rd, int rs, short imm6); 
 void do_str(int rt, int rs, short imm6);
 
 void do_rti();
 
 void do_const(int rd, short imm9);
-void do_sll(int rd, int rs, short uimm4);
-void do_sra(int rd, int rs, short uimm4);
-void do_srl(int rd, int rs, short uimm4);
+
+void do_sll(int rd, int rs, unsigned short uimm4); 
+void do_sra(int rd, int rs, unsigned short uimm4); 
+void do_srl(int rd, int rs, unsigned short uimm4);
 void do_mod(int rd, int rs, int rt);
 
 void do_jmpr(int rs);
 void do_jmp(short imm11);
-void do_hiconst(int rd, short uimm8);
-void do_trap(short uimm8);
+
+void do_hiconst(int rd, unsigned short uimm8);
+
+void do_trap(unsigned short uimm8);
+
+short sext(short n, unsigned short len);
 
 unsigned short parse_instruction(unsigned short word);
 
 void print_lc4_state(FILE *f);
-
